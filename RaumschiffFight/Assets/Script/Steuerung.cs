ac0efef0216192faec884794cc46711ad82a04fa
juz-nst <juz_nst@muffl.de>
2017-06-27 17:01:28 +0200

    Spieler erstellt

---
+++ RaumschiffFight/Assets/Script/Steuerung.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steuerung : MonoBehaviour {
+    public float speed = 5;
+
+
+    void Update () {
+
+        
+            float horizontalePosition = GetComponent<Transform>().position.x + (Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+            float verticalPosition = GetComponent<Transform>().position.y + (Input.GetAxis("Vertical") * Time.deltaTime * speed);
+            GetComponent<Transform>().position = new Vector3(horizontalePosition, verticalPosition, 0);
+
+
+        }
+    }
+
