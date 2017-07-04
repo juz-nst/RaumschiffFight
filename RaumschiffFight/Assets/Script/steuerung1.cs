12fc63250dc17b86dd2d252f634534e640a90211
juz-nst <juz_nst@muffl.de>
2017-07-04 17:54:52 +0200

    fehler1

---
+++ RaumschiffFight/Assets/Script/steuerung1.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic
+ 
+
+
+public class steuerung1 : MonoBehaviour {
+
+    public float speed = 5;
+    Rigidbody2D rb; 
+
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+
+     void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")), ForceMode2D.Force);
+    }
+
+   
+    
+		
+	}
+
